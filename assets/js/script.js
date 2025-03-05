96a829a5c77717614a0b64652b9aaad1e723600c
Algety <108593590+Algety@users.noreply.github.com>
2025-03-05 21:17:24 +0000

    Answer, code, clue done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,45 @@
-// Make draggable elements functional
-function initializeDraggableElements() {
-    const draggables = document.querySelectorAll('.draggable');
-    draggables.forEach(draggable => {
-        draggable.addEventListener('dragstart', function (event) {
-            // Store the ID of the dragged element
-            event.dataTransfer.setData('text/plain', this.id);
-        });
-        
-    });
-    
+// Define listener functions
+function getDragId(event) {
+    // Store the ID of the dragged element
+   event.dataTransfer.setData('text/plain', this.id);
 }
 
 // Define listener functions
 function allowDrop(event) {
-    event.preventDefault(); // Allow the drop
+   event.preventDefault(); // Allow the drop
 }
 
 function handleDrop(event) {
-    document.getElementById("palettBox").innerHTML = "";
-    displayPalett();
-    event.preventDefault();
-
-    // Get the ID of the dragged element
-    const draggedElementId = event.dataTransfer.getData('text/plain');
-    const draggedElement = document.getElementById(draggedElementId);
-
-    // Remove any existing element in the dropzone
-    if (this.firstChild) {
-        this.removeChild(this.firstChild);
-    }
-
-    // Append the dragged element to the dropzone
-    this.appendChild(draggedElement);
+   event.preventDefault();
+   // Get the ID of the dragged element
+   const draggedElementId = event.dataTransfer.getData('text/plain');
+   const draggedElement = document.getElementById(draggedElementId);
+   // Remove any existing element in the dropzone and display a full palett
+   if (this.firstChild) {
+       this.removeChild(this.firstChild);
+       document.getElementById("palettBox").innerHTML = "";
+       displayPalett();
+   }
+   // Append the dragged element to the dropzone with a class
+   this.appendChild(draggedElement);
+   draggedElement.classList.add("added");
+   draggedElement.setAttribute("draggable","false");
+   initializeDraggableElements();
 }
 
+// Make draggable elements functional
+function initializeDraggableElements() {
+    const draggables = document.querySelectorAll('.draggable');
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', getDragId);   
+    });
+}
+
+// Make dropzone elements functional
 function initializeDropzones() {
     const dropzones = document.querySelectorAll('.active');
     dropzones.forEach(dropzone => {
         // Allow dropping on this element
-        dropzone.addEventListener('dragover', allowDrop);
-
-        // Handle the drop event
-        dropzone.addEventListener('drop', handleDrop);
-    });
-}
-
-// Function to make the dropzones in an answerBox active
-function enableDropzones() { 
-
-    const dropzones = digitBox.querySelectorAll('.active');
-    dropzones.forEach(dropzone => {
-        // Allow dropping
         dropzone.addEventListener('dragover', allowDrop);
 
         // Handle the drop event
@@ -74,9 +63,10 @@ function reinitializeDragAndDrop() {
     initializeDropzones();
 }
 
+// * -------------------------------------
+
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-    displayPalett ();
     for (let button of buttons) {
         button.addEventListener("click", function() {
             let clickedButton = this.getAttribute("data-type");
@@ -86,6 +76,10 @@ document.addEventListener("DOMContentLoaded", function() {
                     break;
                 case "start": 
                     difficulty = document.querySelector('[data-type="difficulty"]').textContent;
+                    var gameContainerClass = document.getElementById("gameContainer").classList;
+                    if (gameContainerClass.contains("started")) {
+                        alert("Start a new game?")
+                    };
                     startNewGame(difficulty);
                     break;
                 case "instruction":
@@ -100,6 +94,7 @@ document.addEventListener("DOMContentLoaded", function() {
         initializeDropzones();
     });
     
+// * -----------------------------------------
 
 function difficultySwitch() {
     let difficulty = document.querySelector('[data-type="difficulty"]');
@@ -108,11 +103,17 @@ function difficultySwitch() {
 }
 
 function startNewGame(difficulty) {
+    var gameContainer = document.getElementById("gameContainer");
+    [...gameContainer.children].forEach(child => {
+            child.innerHTML = "";
+        });
+    gameContainer.classList.add("started");
     let code = genCode (difficulty);
     placeCode (code);
-    
-    addAnswerBox();
+    displayPalett ();
+    addAnswerBox ();    
 }
+
 
 function genCode () {
     let codeLength = difficulty === "Easy" ? 4 : 5;
@@ -156,7 +157,7 @@ function addAnswerBox() {
     addNumberOfTries (answerBox);
     addDigitBox (answerBox);
     addSubmitButton (answerBox);
-    // addClueBox ();
+    addClueBox (answerBox);
     return answerBox;
 }
 
@@ -188,60 +189,116 @@ function addSubmitButton (answerBox) {
     const submitButton = document.createElement("button");
     submitButton.className = "answerItem";
     submitButton.id = "submitAnswer";
-    submitButton.setAttribute("data-type","submitButton");
-    submitButton.innerHTML="<i class='fa-solid fa-check'></i>";
+    submitButton.setAttribute("data-type", "submitButton");
+    submitButton.innerHTML = "<i class='fa-solid fa-check'></i>";
+    // Define a named function for the click event
+    function handleClick() {
+        if (isAnswerComplete()) {
+            submitButtonClicked(submitButton, handleClick); // Pass the reference to the handler
+        }
+    }
+    // Add the event listener
+    submitButton.addEventListener("click", handleClick);
     answerBox.appendChild(submitButton);
-    submitButton.addEventListener("click", function () {
-        submitButtonClicked(submitButton);
-    });
-    return submitButton;
+    return submitButton, handleClick;
 }
 
-function submitButtonClicked(submitButton) {
+
+function isAnswerComplete () {
+    const numberOfDigitsInAnswer = [...document.getElementsByClassName("added")].length;
+    const numberOfDigits = document.getElementsByClassName("codeDigit").length;
+    if (numberOfDigitsInAnswer < numberOfDigits) {
+        alert("You need to complete your code");
+    }
+    return numberOfDigitsInAnswer >= numberOfDigits;;
+}
+
+function submitButtonClicked(submitButton, handleClick) {
     submitButton.className = "clicked";
+    submitButton.setAttribute("disabled","true");
+    [...document.getElementsByClassName("added")].forEach (item => {
+        item.classList.remove("added");
+        item.classList.remove("draggable");
+        initializeDraggableElements();
+    })
+    // submitButton.removeEventListener("click", handleClick);
     disableDropzones();
     let activeDigitBoxes = document.getElementsByClassName("active");
     // Use the spread operator to convert to an array
     [...activeDigitBoxes].forEach(activeDigitBox => {
         disableDropzones(activeDigitBox);
+        activeDigitBox.firstChild.classList.add("check");
         activeDigitBox.classList.remove("active");
     });
-    // console.log(activeDigitBoxes);
-    addAnswerBox();
-    // activeAnswerBox = newAnswerBox;
-    // console.log(activeAnswerBox);
-
-    // Enable dropzones for the new active answerBox
     
-    generateClue ();
+    checkAnswer ();
+    displayClue ();
+    addAnswerBox ();
 }
 
-function addClueBox () {
-
+function addClueBox (answerBox) {
+    const clueBox = document.createElement("div");
+    clueBox.className = "answerItem";
+    clueBox.id = "clueBox"
+    answerBox.appendChild(clueBox);
+    addClueDigits (clueBox);
+    return clueBox;
 }
 
-function addNewAnswerBox () {
-
+function addClueDigits (clueBox) {
+    const numberOfDigits = document.getElementsByClassName("codeDigit").length;
+    for (i = 1; i <= numberOfDigits; i++) {
+        let clueDigit = document.createElement("div");
+        clueDigit.textContent = 0;
+        clueDigit.className = "clueDigit activeClue";
+        clueDigit.id = "clueDigit-"+i;
+        clueBox.appendChild(clueDigit);
+    }
 }
 
-function disableAnswerBoxSubmitted () {
+// function getArrayOfAnswer () {
+//         const answer = [];
+//         dropzones.forEach(dropzone => {
+//             answer.push(dropzone.firstChild.textContent);
+//         });
+//         console.log('Content of dropzones:', answer);
+// }
 
-}
+function checkAnswer() {
+    const answerDigit = [...document.getElementsByClassName("check")];
+    const codeDigit = [...document.getElementsByClassName("codeDigit")];
+    const compDigit = [...new Set(codeDigit)];
 
-function getArrayOfAnswer () {
-        const answer = [];
-        dropzones.forEach(dropzone => {
-            answer.push(dropzone.firstChild.textContent);
-        });
-        console.log('Content of dropzones:', answer);
-}
+    var j = 0;
+    var k = 0;
 
-function checkAnswer () {
+    // count digits of the right color and in the right position
+    for (let i = 0; i < codeDigit.length; i++) {
+        if (codeDigit[i].textContent === answerDigit[i].textContent) {
+            j++;
+        }}
+    // count digits of the right color but in the wrong position    
+    for (let i = 0; i < compDigit.length; i++) {
+        let digitToCount = compDigit[i].textContent;
+        let answerOccurance = answerDigit.filter(digit => digit.textContent === digitToCount).length;
+        let codeOccurance = codeDigit.filter(digit => digit.textContent === digitToCount).length;
+        k = k + Math.min(answerOccurance,codeOccurance);
+    } 
+    answerDigit.forEach(item => {
+        item.classList.remove("check");
+    })
+    k = k>=j?k - j:0;
+    displayClue(j,k);
+    }
 
-}
-
-function generateClue () {
-    
+function displayClue (j,k) {
+    const clueDigit = [...document.getElementsByClassName("activeClue")];
+    for (let i = 0; i < j; i++) {
+        clueDigit[i].textContent = 2;
+    }
+    for (let i = j; i < k + j-1; i++) {
+        clueDigit[i].textContent = 1;
+    } 
 }
 
 function showInstruction () {
